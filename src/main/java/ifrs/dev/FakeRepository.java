06f3d6c622b30503336dba56c640a9d8c375dfde
Alexandre Fabian <xandy.fabian@gmail.com>
2022-12-03 18:00:38 -0300

    update on quarkus version

---
+++ src/main/java/ifrs/dev/FakeRepository.java
@@ -0,0 +1,9 @@
+import javax.enterprise.context.ApplicationScoped;
+
+import ifrs.dev.model.Usuario;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+@ApplicationScoped
+public class FakeRepository implements PanacheRepository<Usuario> {
+   //...not necessay implementation
+}
---
+++ target/classes/import.sql
@@ -0,0 +1,2 @@
+#INSERT INTO Usuario(id, login, password, email, admin) VALUES (nextval('hibernate_sequence'),'admin','1234', 'admin@email.com', true);
+
